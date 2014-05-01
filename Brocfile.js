bd7189214ec4f06a4e3722632284bdf6e9b04f48
Carmen Wick <CarmenWick@gmail.com>
2014-05-01 14:14:03 -0700

    Get Jasmine running

--- Brocfile.js
+++ Brocfile.js
@@ -4,27 +4,35 @@ var mergeTrees = require('broccoli-merge-trees');
 var filterES6Modules = require('broccoli-es6-module-filter');
 var pickFiles = require('broccoli-static-compiler');
 
-function resolveBowerDependencies(files) {
-  var dependencies = files.map(function(f) {
-    f = path.join("bower_components", f);
-    return [path.dirname(f), path.basename(f)];
-  });
-
-  return dependencies.map(function(d) {
-    var dirname = d[0];
-    var filename = d[1];
-
-    return pickFiles(dirname, {
-      srcDir: "/",
-      files: [filename],
-      destDir: "/lib"
-    });
-  });
-}
+// function resolveDependencies(files) {
+//   var dependencies = files.map(function(f) {
+//     f = path.join("lib", f);
+//     return [path.dirname(f), path.basename(f)];
+//   });
+//
+//   return dependencies.map(function(d) {
+//     var dirname = d[0];
+//     var filename = d[1];
+//
+//     return pickFiles(dirname, {
+//       srcDir: "/",
+//       files: [filename],
+//       destDir: "/lib"
+//     });
+//   });
+// }
 
 var src = filterCoffeeScript('src', { bare: true });
-var bowerDependencies = [ "gl-matrix/dist/gl-matrix.js" ];
-src = filterES6Modules(src, { moduleType: 'amd' });
-bowerDependencies = resolveBowerDependencies(bowerDependencies);
+var dependencies = pickFiles("lib", { srcDir: "/", destDir: "lib" });
+var specRunner = pickFiles("spec", { srcDir: "/", destDir: "spec", files: ["index.html"] });
+var specLib = pickFiles("spec", { srcDir: "/lib", destDir: "spec/lib" });
+var specFiles = pickFiles("spec", { srcDir: "/src", destDir: "spec/src" });
 
-module.exports = mergeTrees(bowerDependencies.concat(src));
+specFiles = filterCoffeeScript(specFiles, { bare: true });
+specFiles = filterES6Modules(specFiles, { moduleType: "amd" });
+
+spec = [specRunner, specLib, specFiles];
+
+src = filterES6Modules(src, { moduleType: 'amd' });
+
+module.exports = mergeTrees([dependencies, src].concat(spec));
